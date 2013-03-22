43d32421a5b70d56cc1a27770e321bb4c81a07dd
Anton Zhvakin <anton.zhv@gmail.com>
2013-03-22 19:42:50 +0400

    C++: добавлено вычисление левой и правой границы каждой области. Алгоритм основан на анализе гистограмм контрастности и нескольких простых эвристиках. Табличку с номером находит практически всегда.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <functional>
 #include <algorithm>
+#include <utility>
 
 #include <boost/filesystem.hpp>
 #include <boost/format.hpp>
@@ -23,7 +24,7 @@ enum KEY
   RIGHT_KEY = 65361
 };
 
-const std::string folder = "../../test_img/my/";
+const std::string folder = "../../test_img/other/";
 
 boost::filesystem::path path(folder);
 int max_img_index = std::distance(boost::filesystem::directory_iterator(path),
@@ -73,7 +74,7 @@ void right_key_handler()
 }
 
 
-double RMS(const cv::Mat &row)
+double row_RMS(const cv::Mat &row)
 {
   double square_sum = 0.0;
   for (int i = 0; i < row.cols; ++i)
@@ -83,6 +84,18 @@ double RMS(const cv::Mat &row)
   }
 
   return sqrt(square_sum / row.cols);
+}
+
+double col_RMS(const cv::Mat &col)
+{
+  double square_sum = 0.0;
+  for (int i = 0; i < col.rows; ++i)
+  {
+    unsigned char value = col.at<unsigned char>(i, 0);
+    square_sum += (double)value * value;
+  }
+
+  return sqrt(square_sum / col.rows);
 }
 
 
@@ -137,6 +150,108 @@ std::vector<InputIt> find_local_extremums(InputIt first, InputIt last,
 }
 
 
+template<class T>
+std::vector<T> compute_derivative(const std::vector<T> &values, int window_size)
+{
+  std::vector<double> values_der;
+  for (unsigned i = 0; i < values.size() - window_size; ++i)
+  {
+    double sum = 0.0;
+    for (unsigned j = i; j < i + window_size; ++j)
+      sum += values[j + 1] - values[j];
+
+    values_der.push_back(sum);
+  }
+
+  return values_der;
+}
+
+
+void adap_threshold(const cv::Mat &src_img, cv::Mat &dst_img, double thresh)
+{
+  src_img.copyTo(dst_img);
+  for (int i = 1; i < dst_img.cols - 1; ++i)
+    for (int j = 1; j < dst_img.rows - 1; ++j)
+    {
+      double sum = 0.0;
+
+      sum += dst_img.at<unsigned char>(j - 1, i - 1);
+      sum += dst_img.at<unsigned char>(j - 1, i - 0);
+      sum += dst_img.at<unsigned char>(j - 1, i + 1);
+      sum += dst_img.at<unsigned char>(j - 0, i + 1);
+      sum += dst_img.at<unsigned char>(j - 0, i - 0) * 8.0;
+      sum += dst_img.at<unsigned char>(j - 0, i - 1);
+      sum += dst_img.at<unsigned char>(j + 1, i - 1);
+      sum += dst_img.at<unsigned char>(j + 1, i - 0);
+      sum += dst_img.at<unsigned char>(j + 1, i + 1);
+
+      if (sum / 9.0 < thresh)
+        dst_img.at<unsigned char>(j, i) = 0;
+    }
+}
+
+
+template<class InputIt>
+void save_to_file(const std::string &filename, InputIt first, InputIt last)
+{
+  std::ofstream file(filename, std::ios_base::trunc);
+  for (; first != last; ++first)
+    file << *first << std::endl;
+  file.close();
+}
+
+
+struct Area
+{
+  cv::Rect rect;
+  std::vector<double> cols;
+  std::vector<double> cols_der;
+};
+
+
+template<class InputIt>
+std::vector<std::pair<int, int> > compute_local_pairs(InputIt first,
+                                                      InputIt last,
+                                                      int front_offset,
+                                                      int back_offset,
+                                                      double threshold)
+{
+  auto gmax_elem = std::max_element(first + front_offset, last - back_offset);
+  auto gmin_elem = std::min_element(first + front_offset, last - back_offset);
+
+  auto loc_max = find_local_extremums(first + front_offset,
+                                      last - back_offset,
+                                      *gmax_elem, threshold);
+  auto loc_min = find_local_extremums(loc_max.front(),
+                                      last - back_offset,
+                                      *gmin_elem, threshold);
+
+  std::vector<std::pair<int, int>> pairs;
+
+  for (unsigned i = 0; i < loc_max.size() - 1; ++i)
+  {
+    auto max_elem = loc_max[i];
+    auto next_max_elem = loc_max[i + 1];
+    auto min_elem = std::find_if(loc_min.begin(), loc_min.end(),
+                                 [max_elem, next_max_elem](const InputIt &point)
+    {
+      return point > max_elem && point < next_max_elem;
+    });
+
+    if (min_elem != loc_min.end())
+    {
+      std::pair<int, int> pair;
+      pair.first = std::distance(first, max_elem);
+      pair.second = std::distance(first, *min_elem);
+
+      pairs.push_back(pair);
+    }
+  }
+
+  return pairs;
+}
+
+
 void process()
 {
   std::cout << folder + boost::str(boost::format("%03d.jpg") % img_index) << std::endl;
@@ -146,8 +261,8 @@ void process()
   cv::cvtColor(src_img, grayscale_img, CV_RGB2GRAY);
 
   cv::Mat denoised_img;
-  cv::fastNlMeansDenoising(grayscale_img, denoised_img);
-//  denoised_img = grayscale_img;
+//  cv::fastNlMeansDenoising(grayscale_img, denoised_img);
+  denoised_img = grayscale_img;
 
   double m[3][5] = {{-0.2, -1.0, 0.0, 1.0, 0.2},
                     {-0.5, -2.0, 0.0, 2.0, 0.5},
@@ -158,78 +273,87 @@ void process()
   cv::Mat hor_edge_img;
   cv::filter2D(denoised_img, hor_edge_img, -1, edge_matrix.t());
 
+  cv::Mat thresh_vert_edge_img;
+  adap_threshold(vert_edge_img, thresh_vert_edge_img, 120);
+  cv::Mat thresh_hor_edge_img;
+  adap_threshold(hor_edge_img, thresh_hor_edge_img, 120);
+
   std::vector<double> rows;
-  for (int i = 0; i < vert_edge_img.rows; ++i)
-    rows.push_back(RMS(vert_edge_img.row(i)));
+  for (int i = 0; i < thresh_vert_edge_img.rows; ++i)
+    rows.push_back(row_RMS(thresh_vert_edge_img.row(i)));
 
   const int img_top_offset = src_img.rows * 1 / 3;
   const int img_bottom_offset = src_img.rows * 1 / 9;
 
-  const unsigned window_size = 10;
-  std::vector<double> rows_der;
-  for (unsigned i = 0; i < rows.size() - window_size; ++i)
-  {
-    double sum = 0.0;
-    for (unsigned j = i; j < i + window_size; ++j)
-      sum += rows[j + 1] - rows[j];
+  const int window_size = 10;
+  std::vector<double> rows_der = compute_derivative(rows, window_size);
 
-    rows_der.push_back(sum);
+  auto pairs = compute_local_pairs(rows_der.begin(), rows_der.end(),
+                                   img_top_offset, img_bottom_offset, 0.5);
+
+  std::vector<Area> areas;
+  for (auto &pair: pairs)
+  {
+    Area area;
+    pair.first += window_size;
+    pair.second += window_size;
+    area.rect = cv::Rect(0, pair.first, src_img.cols, pair.second - pair.first);
+    areas.push_back(area);
   }
 
-  auto gmax_elem = std::max_element(rows_der.begin() + img_top_offset,
-                                    rows_der.end() - img_bottom_offset);
-  auto gmin_elem = std::min_element(rows_der.begin() + img_top_offset,
-                                    rows_der.end() - img_bottom_offset);
-
-  const double threshold = 0.5;
-
-  auto loc_max = find_local_extremums(rows_der.begin() + img_top_offset,
-                                      rows_der.end() - img_bottom_offset,
-                                      *gmax_elem, threshold);
-  auto loc_min = find_local_extremums(loc_max.front(),
-                                      rows_der.end() - img_bottom_offset,
-                                      *gmin_elem, threshold);
-
-  std::vector<cv::Rect> rects;
-
-  for (unsigned i = 0; i < loc_max.size() - 1; ++i)
+  for (auto &area: areas)
   {
-    auto max_elem = loc_max[i];
-    auto next_max_elem = loc_max[i + 1];
-    auto min_elem = *std::find_if(loc_min.begin(), loc_min.end(),
-                                  [max_elem, next_max_elem](std::vector<double>::iterator point)
+    int min_line = area.rect.y - 10;
+    int max_line = area.rect.y + area.rect.height + 10;
+
+    if (min_line < 0)
+      min_line = 0;
+    if (max_line > thresh_hor_edge_img.rows - 1)
+      max_line = thresh_hor_edge_img.rows - 1;
+
+    cv::Mat rows = thresh_hor_edge_img.rowRange(min_line, max_line);
+    for (int i = 0; i < rows.cols; ++i)
+      area.cols.push_back(col_RMS(rows.col(i)));
+
+    area.cols = simple_moving_average(area.cols, 10, 0.0);
+
+    area.cols_der = compute_derivative(area.cols, 20);
+
+    auto area_pairs = compute_local_pairs(area.cols_der.begin(),
+                                          area.cols_der.end(), 0, 0, 0.5);
+    auto broadest_pair = std::max_element(area_pairs.begin(),
+                                          area_pairs.end(),
+                                          [](const std::pair<int, int> &a,
+                                             const std::pair<int, int> &b)
     {
-      return point > max_elem && point < next_max_elem;
+      return a.second - a.first < b.second - b.first;
     });
 
-    if (max_elem != rows_der.end() && min_elem != rows_der.end())
-    {
-      int max_elem_index = std::distance(rows_der.begin(), max_elem) + window_size * 3 / 4;
-      int min_elem_index = std::distance(rows_der.begin(), min_elem) + window_size * 3 / 4;
+    // yes, it's magic
+    broadest_pair->first += 23;
+    broadest_pair->second += 23;
 
-      rects.push_back(cv::Rect(0, max_elem_index,
-                               src_img.cols,
-                               min_elem_index - max_elem_index));
+    area.rect.x = broadest_pair->first;
+    area.rect.width = broadest_pair->second - broadest_pair->first;
+  }
+
+  {
+    save_to_file("rows", rows.begin(), rows.end());
+    save_to_file("rows_der", rows_der.begin(), rows_der.end());
+
+    for (unsigned i = 0; i < areas.size(); ++i)
+    {
+      save_to_file(boost::str(boost::format("cols_%1%") % i),
+                   areas[i].cols.begin(), areas[i].cols.end());
+      save_to_file(boost::str(boost::format("cols_der_%1%") % i),
+                   areas[i].cols_der.begin(), areas[i].cols_der.end());
     }
   }
 
-  {
-    std::ofstream file;
-    file.open("rows", std::ios_base::trunc);
-    for (auto &row: rows)
-      file << row << std::endl;
-    file.close();
-
-    file.open("rows_der", std::ios_base::trunc);
-    for (auto &row: rows_der)
-      file << row << std::endl;
-    file.close();
-  }
-
-  for (auto &rect: rects)
-    cv::rectangle(src_img, rect, cv::Scalar(255), 2);
+  for (auto &area: areas)
+    cv::rectangle(src_img, area.rect, cv::Scalar(255), 2);
 
   cv::imshow("src_img", src_img);
-  cv::imshow("vert_edge_img", vert_edge_img);
-  cv::imshow("hor_edge_img", hor_edge_img);
+  cv::imshow("thresh_vert_edge_img", thresh_vert_edge_img);
+  cv::imshow("thresh_hor_edge_img", thresh_hor_edge_img);
 }
