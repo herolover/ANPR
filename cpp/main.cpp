09e1168b53c74531113ac013ce482e70c29f7c9c
Anton Zhvakin <anton.zhv@gmail.com>
2013-03-27 16:32:58 +0400

    C++: much improved and simplified the search algorithm.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -79,7 +79,7 @@ double row_RMS(const cv::Mat &row)
   double square_sum = 0.0;
   for (int i = 0; i < row.cols; ++i)
   {
-    unsigned char value = row.at<unsigned char>(0, i);
+    double value = row.at<unsigned char>(0, i) / 255.0;
     square_sum += (double)value * value;
   }
 
@@ -91,18 +91,60 @@ double col_RMS(const cv::Mat &col)
   double square_sum = 0.0;
   for (int i = 0; i < col.rows; ++i)
   {
-    unsigned char value = col.at<unsigned char>(i, 0);
+    double value = col.at<unsigned char>(i, 0) / 255.0;
     square_sum += (double)value * value;
   }
 
   return sqrt(square_sum / col.rows);
 }
 
+double row_sum(const cv::Mat &row)
+{
+  double sum = 0.0;
+  for (int i = 0; i < row.cols; ++i)
+  {
+    double value = row.at<unsigned char>(0, i) / 255.0;
+    sum += value;
+  }
+
+  return sum;
+}
+
+
+double col_sum(const cv::Mat &col)
+{
+  double sum = 0.0;
+  for (int i = 0; i < col.rows; ++i)
+  {
+    double value = col.at<unsigned char>(i, 0) / 255.0;
+    sum += value;
+  }
+
+  return sum;
+}
+
 
 template<class InputIt, class T> inline
-T mean(InputIt first, InputIt last, T init)
+T compute_mean(InputIt first, InputIt last, T init)
 {
   return std::accumulate(first, last, init) / std::distance(first, last);
+}
+
+
+template<class InputIt, class T>
+std::pair<InputIt, InputIt> get_window_elements(const std::vector<T> &values,
+                                                unsigned window_size,
+                                                InputIt element)
+{
+  InputIt first = element - window_size / 2;
+  InputIt last = element + window_size / 2 + 1;
+
+  if (first < values.begin())
+    first = values.begin();
+  if (last > values.end())
+    last = values.end();
+
+  return std::make_pair(first, last);
 }
 
 
@@ -114,24 +156,41 @@ std::vector<T> simple_moving_average(const std::vector<T> &values,
     throw std::runtime_error("SMA: points count must be greater than coverage.");
 
   std::vector<T> smoothed_values;
-  for (auto it = values.begin(); it != values.end() - window_size + 1; ++it)
-    smoothed_values.push_back(mean(it, it + window_size, init));
+  for (auto element = values.begin(); element != values.end(); ++element)
+  {
+    auto window = get_window_elements(values, window_size, element);
+    smoothed_values.push_back(compute_mean(window.first, window.second, init));
+  }
 
   return smoothed_values;
 }
 
 
+template<class T>
+std::vector<T> exp_smooth(const std::vector<T> &values, double alpha)
+{
+  std::vector<T> smoothed_values;
+  smoothed_values.push_back(values.front());
+  for (auto element = values.begin() + 1; element != values.end(); ++element)
+    smoothed_values.push_back(smoothed_values.back() + alpha
+                              * ((*element) - smoothed_values.back()));
+
+  return smoothed_values;
+}
+
+
+
 template<class InputIt>
-InputIt find(InputIt first, InputIt last, InputIt global, double threshold,
-             const std::vector<std::pair<InputIt, InputIt>> &ignored_ranges)
+InputIt find_local_maximum(InputIt first, InputIt last, double threshold,
+                           const std::vector<std::pair<InputIt, InputIt>> &ignored_ranges)
 {
   bool is_inited = false;
-  InputIt loc_ext = last;
+  InputIt local_maximum = last;
   for (; first != last; ++first)
   {
     bool ignore_value = false;
 
-    if ((*first) / (*global) < threshold)
+    if (*first < threshold)
       ignore_value = true;
 
     for (auto &range: ignored_ranges)
@@ -142,52 +201,85 @@ InputIt find(InputIt first, InputIt last, InputIt global, double threshold,
       }
 
     if (!ignore_value)
-      if (!is_inited || (*first) / (*loc_ext) > 1.0)
+      if (!is_inited || *first > *local_maximum)
       {
-        loc_ext = first;
+        local_maximum = first;
         is_inited = true;
       }
   }
 
-  return loc_ext;
+  return local_maximum;
 }
 
 
 template<class InputIt>
-std::vector<InputIt> find_local_extremums(InputIt first, InputIt last,
-                                          InputIt global, double threshold)
+std::pair<InputIt, InputIt> get_threshold_bound(InputIt first, InputIt last,
+                                                InputIt element, double threshold)
 {
-  std::vector<InputIt> local_extremums;
+  InputIt left = element - 1;
+  for (; left >= first; --left)
+    if (*left < threshold)
+      break;
 
-  std::vector<std::pair<InputIt, InputIt>> ignored_ranges;
+  InputIt right = element + 1;
+  for (; right < last; ++right)
+    if (*right < threshold)
+      break;
 
-  InputIt local_extremum = first;
-  while (local_extremum != last)
+  return std::make_pair(left, right);
+}
+
+
+template<class InputIt>
+std::vector<std::pair<InputIt, InputIt> > find_local_pairs(InputIt first,
+                                                           InputIt last,
+                                                           double threshold)
+{
+  auto global_maximum = std::max_element(first, last);
+//  auto global_minimum = std::min_element(first, last);
+  auto mean = compute_mean(first, last, 0.0);
+  double abs_threshold = (*global_maximum - mean) * threshold + mean;
+  std::cout << abs_threshold << std::endl;
+
+  std::vector<std::pair<InputIt, InputIt> > local_pairs;
+  std::vector<std::pair<InputIt, InputIt> > ignored_ranges;
+
+  while (true)
   {
-    local_extremum = find(first, last, global, threshold, ignored_ranges);
+    InputIt local_maximum = find_local_maximum(first, last, abs_threshold,
+                                               ignored_ranges);
 
-    local_extremums.push_back(local_extremum);
-    ignored_ranges.push_back(std::make_pair(local_extremum - 20,
-                                            local_extremum + 20));
+    if (local_maximum != last)
+    {
+      auto bound = get_threshold_bound(first, last, local_maximum, abs_threshold);
+
+      ignored_ranges.push_back(bound);
+      if (bound.first != first && bound.second != last)
+        local_pairs.push_back(bound);
+    }
+    else
+      break;
   }
 
-  std::sort(local_extremums.begin(), local_extremums.end());
-
-  return local_extremums;
+  return local_pairs;
 }
 
 
 template<class T>
-std::vector<T> compute_derivative(const std::vector<T> &values, int window_size)
+std::vector<T> compute_derivative(const std::vector<T> &values, int window_size,
+                                  T init)
 {
   std::vector<double> values_der;
-  for (unsigned i = 0; i < values.size() - window_size; ++i)
-  {
-    double sum = 0.0;
-    for (unsigned j = i; j < i + window_size; ++j)
-      sum += values[j + 1] - values[j];
 
-    values_der.push_back(sum);
+  for (auto element = values.begin(); element != values.end(); ++element)
+  {
+    auto window = get_window_elements(values, window_size, element);
+
+    T sum = init;
+    for (auto it = window.first + 1; it != window.second; ++it)
+      sum += *it - *(it - 1);
+
+    values_der.push_back(sum);// / std::distance(window.first, window.second));
   }
 
   return values_der;
@@ -232,54 +324,12 @@ struct Area
 {
   cv::Rect rect;
   std::vector<double> cols;
-  std::vector<double> cols_der;
+
+  cv::Mat src_img;
+  cv::Mat grayscale_img;
+  cv::Mat hor_img;
+  cv::Mat vert_img;
 };
-
-
-template<class InputIt>
-std::vector<std::pair<int, int> > find_local_pairs(InputIt first,
-                                                   InputIt last,
-                                                   double threshold)
-{
-  auto global_max_element = std::max_element(first, last);
-  auto global_min_element = std::min_element(first, last);
-
-  auto local_maximums = find_local_extremums(first, last,
-                                             global_max_element, threshold);
-  auto local_minimums = find_local_extremums(first, last,
-                                             global_min_element, threshold);
-
-  std::vector<std::pair<int, int>> pairs;
-
-  for (unsigned i = 0; i < local_maximums.size() - 1; ++i)
-  {
-    auto max_element = local_maximums[i];
-
-    for (unsigned j = i + 1; j < local_maximums.size(); ++j)
-    {
-      auto next_max_element = local_maximums[j];
-      auto min_element = std::find_if(local_minimums.begin(),
-                                      local_minimums.end(),
-                                      [max_element, next_max_element](const InputIt &element)
-      {
-        return element > max_element && element < next_max_element;
-      });
-
-      if (min_element != local_minimums.end())
-      {
-        std::pair<int, int> pair;
-        pair.first = std::distance(first, max_element);
-        pair.second = std::distance(first, *min_element);
-
-        pairs.push_back(pair);
-
-        break;
-      }
-    }
-  }
-
-  return pairs;
-}
 
 
 void process()
@@ -294,104 +344,77 @@ void process()
 //  cv::fastNlMeansDenoising(grayscale_img, denoised_img);
   denoised_img = grayscale_img;
 
-  double m[3][5] = {{-0.2, -1.0, 0.0, 1.0, 0.2},
-                    {-0.5, -2.0, 0.0, 2.0, 0.5},
-                    {-0.2, -1.0, 0.0, 1.0, 0.2}};
-  cv::Mat edge_matrix(3, 5, CV_64FC1, m);
+  double m[3][3] = {{-1.0, 0.0, 1.0},
+                    {-2.0, 0.0, 2.0},
+                    {-1.0, 0.0, 1.0}};
+  cv::Mat edge_matrix(3, 3, CV_64FC1, m);
   cv::Mat vert_edge_img;
   cv::filter2D(denoised_img, vert_edge_img, -1, edge_matrix);
   cv::Mat hor_edge_img;
   cv::filter2D(denoised_img, hor_edge_img, -1, edge_matrix.t());
 
   cv::Mat thresh_vert_edge_img;
-  adap_threshold(vert_edge_img, thresh_vert_edge_img, 120);
+  adap_threshold(vert_edge_img, thresh_vert_edge_img, 150);
   cv::Mat thresh_hor_edge_img;
-  adap_threshold(hor_edge_img, thresh_hor_edge_img, 120);
+  adap_threshold(hor_edge_img, thresh_hor_edge_img, 150);
 
   std::vector<double> rows;
   for (int i = 0; i < thresh_vert_edge_img.rows; ++i)
     rows.push_back(row_RMS(thresh_vert_edge_img.row(i)));
 
-  rows = simple_moving_average(rows, 5, 0.0);
+  rows = simple_moving_average(rows, 10, 0.0);
 
-  const int img_top_offset = src_img.rows * 1 / 3;
+  save_to_file("rows", rows.begin(), rows.end());
+
+  const int img_top_offset = src_img.rows * 1 / 4;
   const int img_bottom_offset = src_img.rows * 1 / 9;
-
-  const int window_size = 10;
-  std::vector<double> rows_der = compute_derivative(rows, window_size);
-  rows_der = simple_moving_average(rows_der, 5, 0.0);
-
-  auto pairs = find_local_pairs(rows_der.begin() + img_top_offset,
-                                rows_der.end() - img_bottom_offset,
-                                0.5);
+  auto row_pairs = find_local_pairs(rows.begin() + img_top_offset,
+                                    rows.end() - img_bottom_offset,
+                                    0.4);
 
   std::vector<Area> areas;
-  for (auto &pair: pairs)
+  for (unsigned i = 0; i < row_pairs.size(); ++i)
   {
+    int top_bound = std::distance(rows.begin(), row_pairs[i].first) - 5;
+    int bottom_bound = std::distance(rows.begin(), row_pairs[i].second) + 5;
+
+    std::cout << top_bound << " " << bottom_bound << std::endl;
+
+    thresh_vert_edge_img = thresh_hor_edge_img;
+
     Area area;
-    pair.first += window_size + img_top_offset;
-    pair.second += window_size + img_top_offset;
-    area.rect = cv::Rect(0, pair.first, src_img.cols, pair.second - pair.first);
-    areas.push_back(area);
-  }
-
-  std::cout << std::endl;
-
-  for (auto &area: areas)
-  {
-    int min_line = area.rect.y - 5;
-    int max_line = area.rect.y + area.rect.height + 5;
-
-    if (min_line < 0)
-      min_line = 0;
-    if (max_line > thresh_hor_edge_img.rows - 1)
-      max_line = thresh_hor_edge_img.rows - 1;
-
-    cv::Mat rows = thresh_hor_edge_img.rowRange(min_line, max_line);
+    cv::Mat rows = thresh_vert_edge_img.rowRange(top_bound, bottom_bound);
     for (int i = 0; i < rows.cols; ++i)
       area.cols.push_back(col_RMS(rows.col(i)));
 
-    area.cols = simple_moving_average(area.cols, 10, 0.0);
+    area.cols = simple_moving_average(area.cols, 50, 0.0);
 
-    area.cols_der = compute_derivative(area.cols, 20);
+    save_to_file(boost::str(boost::format("cols_%1%") % i),
+                 area.cols.begin(), area.cols.end());
 
-    const int img_offset_left = src_img.cols / 5;
-    const int img_offset_right = src_img.cols / 5;
-    auto area_pairs = find_local_pairs(area.cols_der.begin() + img_offset_left,
-                                       area.cols_der.end() - img_offset_right,
-                                       0.5);
+    const int img_offset_left = src_img.cols / 8;
+    const int img_offset_right = src_img.cols / 8;
+    auto col_pairs = find_local_pairs(area.cols.begin() + img_offset_left,
+                                      area.cols.end() - img_offset_right,
+                                      0.05);
 
-    if (area_pairs.size() > 0)
+
+    for (auto &col_pair: col_pairs)
     {
-      auto broadest_pair = std::max_element(area_pairs.begin(),
-                                            area_pairs.end(),
-                                            [](const std::pair<int, int> &a,
-                                               const std::pair<int, int> &b)
-      {
-        return a.second - a.first < b.second - b.first;
-      });
+      int left_bound = std::distance(area.cols.begin(), col_pair.first) - 5;
+      int right_bound = std::distance(area.cols.begin(), col_pair.second) + 5;
 
-      // yes, it's magic
-      broadest_pair->first += 15 + img_offset_left;
-      broadest_pair->second += 15 + img_offset_left;
+      area.rect.x = left_bound;
+      area.rect.y = top_bound;
+      area.rect.height = bottom_bound - top_bound;
+      area.rect.width = right_bound - left_bound;
 
-      std::cout << broadest_pair->first << " " << broadest_pair->second << std::endl;
+      area.src_img = src_img(area.rect);
+      area.grayscale_img = grayscale_img(area.rect);
+      area.hor_img = thresh_hor_edge_img(area.rect);
+      area.vert_img = thresh_vert_edge_img(area.rect);
 
-      area.rect.x = broadest_pair->first;
-      area.rect.width = broadest_pair->second - broadest_pair->first;
-    }
-  }
-
-  {
-    save_to_file("rows", rows.begin(), rows.end());
-    save_to_file("rows_der", rows_der.begin(), rows_der.end());
-
-    for (unsigned i = 0; i < areas.size(); ++i)
-    {
-      save_to_file(boost::str(boost::format("cols_%1%") % i),
-                   areas[i].cols.begin(), areas[i].cols.end());
-      save_to_file(boost::str(boost::format("cols_der_%1%") % i),
-                   areas[i].cols_der.begin(), areas[i].cols_der.end());
+      areas.push_back(area);
     }
   }
 
@@ -399,6 +422,9 @@ void process()
     cv::rectangle(src_img, area.rect, cv::Scalar(255), 2);
 
   cv::imshow("src_img", src_img);
+  if (areas.size() > 0)
+    cv::imshow("plate", areas[0].src_img);
+
   cv::imshow("thresh_vert_edge_img", thresh_vert_edge_img);
-  cv::imshow("thresh_hor_edge_img", thresh_hor_edge_img);
+//  cv::imshow("thresh_hor_edge_img", thresh_hor_edge_img);
 }
